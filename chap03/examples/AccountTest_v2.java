fe84fdeec97bf4f23e270e7af135e1c7d9b4fb07
biraneves <ubirajara.neves@usp.br>
2022-06-30 19:26:30 -0300

    Class Account with constructor.

---
+++ chap03/examples/AccountTest_v2.java
@@ -0,0 +1,13 @@
+class AccountTest {
+
+    public static void main(String [] args) {
+
+        Account account1 = new Account("Jane Green");
+        Account account2 = new Account("John Blue");
+
+        System.out.printf("account1 name: %s%n", account1.getName());
+        System.out.printf("account2 name: %s%n", account2.getName());
+
+    }
+
+}
---
+++ chap03/examples/Account_v2.java
@@ -0,0 +1,23 @@
+class Account {
+
+    private String name;
+
+    public Account(String name) {
+
+        this.name = name;
+
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+    public String getName() {
+
+        return name;
+
+    }
+
+}
